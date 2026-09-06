d89d71a30c89009c1837ceaf2f55319274513ab7
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-06 15:58:46 +0200

    #10 (feat, config): routing impl, edit  client.ts structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BookingPage } from "./pages/BookingPage";
 
+// TODO temp
+const MyBookingsPage = () => (
+  <div style={{ padding: 20 }}>Saját foglalásaim oldal</div>
+);
+
+export const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/booking" replace />} />
 
-export default App;
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/my-bookings" element={<MyBookingsPage />} />
+
+        <Route
+          path="*"
+          element={<div style={{ padding: 20 }}>Az oldal nem található!</div>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
--- frontend/src/api/clients.ts
+++ frontend/src/api/clients.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+
+const apiKey = import.meta.env.VITE_APP_API_KEY;
+
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   headers: {
-    "x-api-key": import.meta.env.VITE_APP_API_KEY,
+    "x-api-key": apiKey,
   },
 });
