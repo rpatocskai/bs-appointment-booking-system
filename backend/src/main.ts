3a696fd5779d1f44e1371014e95a2ef7ffc34210
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-06 10:38:23 +0200

    #10 (config): CORS policy for FE

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>(
+    'FRONTEND_URL',
+    'http://localhost:5173',
+  );
+
+  app.enableCors({
+    origin: frontendUrl,
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
