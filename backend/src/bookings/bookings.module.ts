e675d41fa106a2024450728d054c6f2999760a4a
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-07 17:35:56 +0200

    #17 (feat): impl business-day validation

--- backend/src/bookings/bookings.module.ts
+++ backend/src/bookings/bookings.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { BookingsController } from './controllers/bookings.controller.js';
 import { BookingsService } from './services/bookings.service.js';
 import { JsonBookingRepository } from './infrastructure/json-booking-repository.js';
+import { OpeningHoursValidator } from './validators/opening-hours.validator.js';
+import { BusinessDayValidator } from './validators/business-day.validator.js';
 
 @Module({
   controllers: [BookingsController],
   providers: [
     BookingsService,
+    OpeningHoursValidator,
+    BusinessDayValidator,
     { provide: 'IBookingRepository', useClass: JsonBookingRepository },
   ],
-  exports: [BookingsService, 'IBookingRepository'],
+  exports: [BookingsService],
 })
 export class BookingsModule {}
--- backend/src/bookings/services/bookings.service.ts
+++ backend/src/bookings/services/bookings.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import * as bookingRepositoryInterface from '../domains/booking.repository.interface.js';
+import { BusinessDayValidator } from '../validators/business-day.validator.js';
 import { OpeningHoursValidator } from '../validators/opening-hours.validator.js';
 
 @Injectable()
@@ -8,6 +9,7 @@ export class BookingsService {
     @Inject('IBookingRepository')
     private readonly bookingRepository: bookingRepositoryInterface.IBookingRepository,
     private readonly openingHoursValidator: OpeningHoursValidator,
+    private readonly businessDayValidator: BusinessDayValidator,
   ) {}
 
   async validateBookingTimes(
@@ -17,6 +19,7 @@ export class BookingsService {
     const start = new Date(startTimeStr);
     const end = new Date(endTimeStr);
 
+    this.businessDayValidator.validate(start);
     this.openingHoursValidator.validate(start, end);
   }
 }
