33ad871a19d53b1850fd03dc2f7cfddd4e1412a8
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-07 17:20:21 +0200

    #19 (feat): impl openincs hours validation

--- backend/src/bookings/services/bookings.service.ts
+++ backend/src/bookings/services/bookings.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import * as bookingRepositoryInterface from '../domains/booking.repository.interface.js';
+import { OpeningHoursValidator } from '../validators/opening-hours.validator.js';
 
 @Injectable()
-export class BookingsService {}
+export class BookingsService {
+  constructor(
+    @Inject('IBookingRepository')
+    private readonly bookingRepository: bookingRepositoryInterface.IBookingRepository,
+    private readonly openingHoursValidator: OpeningHoursValidator,
+  ) {}
+
+  async validateBookingTimes(
+    startTimeStr: string,
+    endTimeStr: string,
+  ): Promise<void> {
+    const start = new Date(startTimeStr);
+    const end = new Date(endTimeStr);
+
+    this.openingHoursValidator.validate(start, end);
+  }
+}
