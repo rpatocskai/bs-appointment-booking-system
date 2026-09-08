203791920442b8145d6444be1eb9402d6459a3db
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-08 18:43:59 +0200

    #29, #30 (feat, test):  impl bookings post with validation and tests

--- backend/src/bookings/controllers/bookings.controller.ts
+++ backend/src/bookings/controllers/bookings.controller.ts
@@ -1,6 +1,11 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
+  Post,
   Query,
   UseGuards,
   UsePipes,
@@ -9,6 +14,7 @@ import {
 import { BookingsService } from '../services/bookings.service.js';
 import { AvailabilityDto } from '../dtos/availability.dto.js';
 import { ApiKeyGuard } from '../../common/guards/api-key.guard.js';
+import { CreateBookingDto } from '../dtos/create-booking.dto.js';
 
 @Controller('bookings')
 @UseGuards(ApiKeyGuard)
@@ -19,5 +25,17 @@ export class BookingsController {
   @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
   async getAvailability(@Query() query: AvailabilityDto) {
     return this.bookingsService.getAvailability(query);
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
+  async createBooking(@Body() createBookingDto: CreateBookingDto) {
+    return this.bookingsService.createBooking(createBookingDto);
+  }
+
+  @Delete()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteAllBookings() {
+    await this.bookingsService.deleteAllBookings();
   }
 }
