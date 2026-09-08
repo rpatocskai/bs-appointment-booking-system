31addd1a473eb9f46c59b3cf6eb81cdcd445c851
rpatocskai <rpatocskai.developer@gmail.com>
2026-09-08 21:44:13 +0200

    #32, #33 (feat): add booking success state and confirmation, update read.me

--- frontend/src/features/bookings/types/booking.types.ts
+++ frontend/src/features/bookings/types/booking.types.ts
@@ -7,3 +7,10 @@ export interface AvailabilityQueryParams {
   barberId: string;
   date: string;
 }
+
+export interface CreateBookingPayload {
+  barberId: string;
+  customerEmail: string;
+  startTime: string; // ISO String
+  endTime: string; // ISO String
+}
